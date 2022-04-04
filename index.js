5c7d4e4f43e13dfc60e400714c4eb0daf73b0766
brian <riceb53@gmail.com>
2022-04-04 10:46:19 -0700

    add webpack

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+var moment = require('moment'); 
+
 console.log('in javascript')
 
 console.log(moment().format("MMM Do YY"));
+
+
+
+
+
+// js
+// can't access files, because it runs on other people's computers
+
+
+// nodejs
+// can access files, cannot run in browser
+
+
+// ruby
+// has access to files, cannot run in browser
