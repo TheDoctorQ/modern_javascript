b22b8e377d951c875fbbf4c3a893edb465fc0337
brian <riceb53@gmail.com>
2022-04-04 11:05:34 -0700

    add chance js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var moment = require('moment'); 
+var Chance = require('chance');
 
 console.log('in javascript')
 
@@ -7,7 +8,14 @@ console.log(moment().format("MMM Do YY"));
 
 
 
+// Instantiate Chance so it can be used
+var chance = new Chance();
 
+// Use Chance here.
+var my_random_string = chance.string();
+console.log(my_random_string);
+
+console.log('HELLOOOOOOO')
 // js
 // can't access files, because it runs on other people's computers
 
